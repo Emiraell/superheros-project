de0e90572e1deda1f59f2ead0dfd7e7f563892da
emiraelDev <emmzex19@gmail.com>
2023-07-25 12:42:14 +0100

    my pratice html

--- scripts/index.js
+++ scripts/index.js
@@ -8,23 +8,53 @@ document.getElementById('randomHero')
   getRandomHero ();
 })
 
+
 const apiUrl = 'https://superheroapi.com/api.php'
 const myAccessToken = '2480652435442335'
+const heroImgStat = document.getElementById('heroImgStats')
 
-function getRandomHero () {
-  const randomHeroId = Math.random() * 731
-  const heroId = Math.floor(randomHeroId) + 1
-  console.log(heroId)
-  const baseUrl = `${apiUrl}/${myAccessToken}/${heroId}`
 
-  fetch(baseUrl)
-  .then(response => response.json())
-  .then(json => {
-    
-    console.log (json)
-  })
+async function getRandomHero () {
+
+  const randomHero = Math.random() * 731
+  const herosId = Math.floor(randomHero) + 1
+  console.log(herosId)
+
+  const baseUrl = `${apiUrl}/${myAccessToken}/${herosId}`
+
+  let powerstatsEmoji = {
+    intelligence : '🧠',
+    strength : '📶',
+    speed : '🏃',
+    durability : '📊',
+    power: '🦾',
+    combat: '⚔️'
+  }
   
-  console.log('Got random Hero')
+  
+try {
+   let response = await fetch (baseUrl)
+   let data = await response.json()
+   
+   console.log (data)
+   
+    const heroImage = data.image.url
+    let powerStatHtml = ''
+    console.log(data.name)
+    console.log(Object.keys(data.powerstats))
+    
+    Object.keys(data.powerstats).forEach ((stat) => {
+      
+      powerStatHtml += `<p> ${powerstatsEmoji[stat]} ${stat} : ${data.powerstats[stat]}</p>`
+      console.log(powerStatHtml)
+    });
+    heroImgStat.innerHTML = `
+      <img src = "${heroImage}" height = 200 width= 200>
+`
+    }
+  catch {
+    console.log ('error')
+  }
 }
 
 
