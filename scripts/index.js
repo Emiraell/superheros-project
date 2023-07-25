b3f0c2bf8276657f32aeb4388998c37ea8abde34
emiraelDev <emmzex19@gmail.com>
2023-07-25 14:59:30 +0100

    hero profile improvement

--- scripts/index.js
+++ scripts/index.js
@@ -14,6 +14,7 @@ const myAccessToken = '2480652435442335'
 const heroImgStat = document.getElementById('heroImgStats')
 const powerstatDiv = document.querySelector('.js-heroInfo')
 const heroNameLm = document.getElementById('heroName')
+const heroProfileDiv = document.querySelector('.js-heroProfile')
 
 
 async function getRandomHero () {
@@ -39,8 +40,8 @@ try {
    let data = await response.json()
    
    
-   /*console.log (data)
-   console.log(data.biography['full-name'])
+   //console.log (data)
+   /*console.log(data.biography['full-name'])
    console.log(data.biography['place-of-birth'])
    console.log(data.appearance.gender)
    console.log(data.appearance.height[1])
@@ -51,29 +52,35 @@ try {
    
     const heroImage = data.image.url
     let powerStatHtml = ''
+    let profileHtml = ''
     let heroProfile = {
       ['full-name'] : data.biography['full-name'],
       gender: data.appearance.gender,
       ['place-of-birth']:data.biography['place-of-birth'],
+      ['first-appearance'] : data.biography['first-appearance'],
       alignment : data.biography.alignment,
-      ['alter-egos'] : data.biography['alter-egos'],
       height : data.appearance.height[1],
       weight : data.appearance.weight[1]
     }
     Object.keys(heroProfile).forEach ((profile) => {
-      console.log(`${profile} : ${heroProfile[profile]}`)
-    })
+      profileHtml += `<p> ${profile} : ${heroProfile[profile]}</p>`
+      //console.log(profileHtml)
+    });
     //let heroNamee = data.name
    // console.log(Object.keys(data.powerstats))
     
     Object.keys(data.powerstats).forEach ((stat) => {
       
-      powerStatHtml += `<p> ${powerstatsEmoji[stat]} ${stat.toLocaleUpperCase()} : ${data.powerstats[stat]}</p>`
+      powerStatHtml += `
+        <p> ${powerstatsEmoji[stat]} ${stat.toLocaleUpperCase()} : ${data.powerstats[stat]}</p>`
       //console.log(powerStatHtml)
     });
     heroNameLm.innerHTML = data.name
     heroImgStat.innerHTML = `<img src = "${heroImage}" height = 200 width= 200>`
+    heroProfileDiv.innerHTML = profileHtml
     powerstatDiv.innerHTML = powerStatHtml
+    
+    
     
     
     }
