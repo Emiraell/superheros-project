34e744069608ad29cd4d5228b739e92d7f2f2789
emiraelDev <emmzex19@gmail.com>
2023-07-25 17:52:08 +0100

    search hero by name

--- scripts/index.js
+++ scripts/index.js
@@ -16,80 +16,144 @@ const powerstatDiv = document.querySelector('.js-heroInfo')
 const heroNameLm = document.getElementById('heroName')
 const heroProfileDiv = document.querySelector('.js-heroProfile')
 
+let powerstatsEmoji = {
+  intelligence : '🧠',
+  strength : '📶',
+  speed : '🏃', 
+  durability : '📊',
+  power: '🦾',
+  combat: '⚔️'
+}
 
-async function getRandomHero () {
+
+ async function getRandomHero () {
 
   const randomHero = Math.random() * 731
   const herosId = Math.floor(randomHero) + 1
   //console.log(herosId)
 
   const baseUrl = `${apiUrl}/${myAccessToken}/${herosId}`
-
-  let powerstatsEmoji = {
-    intelligence : '🧠',
-    strength : '📶',
-    speed : '🏃',
-    durability : '📊',
-    power: '🦾',
-    combat: '⚔️'
-  }
-  
   
 try {
    let response = await fetch (baseUrl)
-   let data = await response.json()
+   let hero = await response.json()
+   let superHero = hero
+   superHeroRender (superHero)
    
-   
-   //console.log (data)
-   /*console.log(data.biography['full-name'])
-   console.log(data.biography['place-of-birth'])
-   console.log(data.appearance.gender)
-   console.log(data.appearance.height[1])
-   console.log(data.appearance.weight[1])
-   console.log(data.biography.alignment)
-   console.log(data.biography['alter-egos'])*/
-   //data.biography.aliases.forEach((person) => {console.log(person += '' )})
-   
-    const heroImage = data.image.url
+   /* const heroImagee = hero.image.url
     let powerStatHtml = ''
     let profileHtml = ''
     let heroProfile = {
-      ['full-name'] : data.biography['full-name'],
-      gender: data.appearance.gender,
-      ['place-of-birth']:data.biography['place-of-birth'],
-      ['first-appearance'] : data.biography['first-appearance'],
-      alignment : data.biography.alignment,
-      height : data.appearance.height[1],
-      weight : data.appearance.weight[1]
+      ['full-name'] : hero.biography['full-name'],
+      gender: hero.appearance.gender,
+      ['place-of-birth']:hero.biography['place-of-birth'],
+      ['first-appearance'] : hero.biography['first-appearance'],
+      alignment : hero.biography.alignment,
+      height : hero.appearance.height[1],
+      weight : hero.appearance.weight[1]
     }
     Object.keys(heroProfile).forEach ((profile) => {
       profileHtml += `<p> ${profile} : ${heroProfile[profile]}</p>`
-      //console.log(profileHtml)
     });
-    //let heroNamee = data.name
-   // console.log(Object.keys(data.powerstats))
     
-    Object.keys(data.powerstats).forEach ((stat) => {
+    Object.keys(hero.powerstats).forEach ((stat) => {
       
       powerStatHtml += `
-        <p> ${powerstatsEmoji[stat]} ${stat.toLocaleUpperCase()} : ${data.powerstats[stat]}</p>`
-      //console.log(powerStatHtml)
+        <p> ${powerstatsEmoji[stat]} ${stat.toLocaleUpperCase()} : ${hero.powerstats[stat]}</p>`
     });
-    heroNameLm.innerHTML = data.name
-    heroImgStat.innerHTML = `<img src = "${heroImage}" height = 200 width= 200>`
+    heroNameLm.innerHTML = hero.name
+    heroImgStat.innerHTML = `<img src = "${heroImagee}" height = 200 width= 200>`
     heroProfileDiv.innerHTML = profileHtml
-    powerstatDiv.innerHTML = powerStatHtml
-    
-    
-    
+    powerstatDiv.innerHTML = powerStatHtml*/
     
     }
   catch {
     console.log ('error')
   }
+  
 }
 
 
-function searchHero () {
-  console.log('serached Hero')
+
+const superHeroRender = (hero) => {
+
+    
+     const heroImagee = hero.image.url
+     let powerStatHtml = ''
+     let profileHtml = ''
+     let heroProfile = {
+       ['full-name'] : hero.biography['full-name'],
+       gender: hero.appearance.gender,
+       ['place-of-birth']:hero.biography['place-of-birth'],
+       ['first-appearance'] : hero.biography['first-appearance'],
+       alignment : hero.biography.alignment,
+       height : hero.appearance.height[1],
+       weight : hero.appearance.weight[1]
+     }
+     Object.keys(heroProfile).forEach ((profile) => {
+       profileHtml += `<p> ${profile} : ${heroProfile[profile]}</p>`
+     });
+     
+     Object.keys(hero.powerstats).forEach ((stat) => {
+       
+       powerStatHtml += `
+         <p> ${powerstatsEmoji[stat]} ${stat.toLocaleUpperCase()} : ${hero.powerstats[stat]}</p>`
+     });
+     heroNameLm.innerHTML = hero.name
+     heroImgStat.innerHTML = `<img src = "${heroImagee}" height = 200 width= 200>`
+     heroProfileDiv.innerHTML = profileHtml
+     powerstatDiv.innerHTML = powerStatHtml
+ 
+}
+
+
+async function searchHero () {
+
+  const heroSearch = document.querySelector('.js-heroInput')
+  const baseUrl = `${apiUrl}/${myAccessToken}/search/${heroSearch.value}`
+  //console.log(baseUrl)
+
+  
+  try {
+    let response = await fetch (baseUrl)
+    let hero = await response.json()
+    let superHero = hero.results[0]
+
+    superHeroRender(superHero)
+    heroSearch.value = ''
+
+
+    
+    
+     /*const heroImage = hero.results[0].image.url
+     //console.log(heroImage)
+     let powerStatHtml = ''
+     let profileHtml = ''
+     let heroProfile = {
+       ['full-name'] : hero.results[0].biography['full-name'],
+       gender: hero.results[0].appearance.gender,
+       ['place-of-birth']:hero.results[0].biography['place-of-birth'],
+       ['first-appearance'] : hero.results[0].biography['first-appearance'],
+       alignment : hero.results[0].biography.alignment,
+       height : hero.results[0].appearance.height[1],
+       weight : hero.results[0].appearance.weight[1]
+     }
+     Object.keys(heroProfile).forEach ((profile) => {
+       profileHtml += `<p> ${profile.toUpperCase()} : ${heroProfile[profile]}</p>`
+     });
+     
+     Object.keys(hero.results[0].powerstats).forEach ((stat) => {
+       
+       powerStatHtml += `
+         <p> ${powerstatsEmoji[stat]} ${stat.toLocaleUpperCase()} : ${hero.results[0].powerstats[stat]}</p>`
+     });
+     heroNameLm.innerHTML = hero.results[0].name
+     heroImgStat.innerHTML = `<img src = "${heroImage}" height = 200 width= 200>`
+     heroProfileDiv.innerHTML = profileHtml
+     powerstatDiv.innerHTML = powerStatHtml*/
+     
+     }
+   catch {
+     console.log ('error')
+   }
 }
